008d102d18894f568df7000a68eccd1678d94de3
Akshaj05 <akshajramakrishnan@gmail.com>
2025-04-12 22:04:24 +0530

    Started Disconnect vent listner part

--- src/main/java/com/example/demo/config/WebSocketEventListener.java
+++ src/main/java/com/example/demo/config/WebSocketEventListener.java
@@ -3,6 +3,8 @@ package com.example.demo.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,8 +12,21 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j //for logging when user leaves chat
 public class WebSocketEventListener {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
     @EventListener //to listen on the given event
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
         //TO DO inform users of the chat app that someone has left the chat
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null){
+            log.info("User Disconnected: " + username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public",chatMessage);
+        }
     }
 }
